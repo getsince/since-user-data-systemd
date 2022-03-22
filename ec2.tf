c38e742a5ec8458e50735425c200b8f8a8a8c218
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2022-03-22 22:45:22 +0300

    fix db urls for regions other than stockholm

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,10 @@
 # TODO t4g with erlang-25
 
-locals {
+module "ec2_stockholm" {
+  source = "./ec2"
+
+  instance_name = var.ec2_name
+
   user_data_base64 = base64encode(templatefile("user_data.sh.tftpl", {
     release_url           = var.release_url,
     release_cookie        = var.release_cookie,
@@ -36,13 +40,7 @@ locals {
     phone_home_tg_bot_key = var.phone_home_tg_bot_key,
     phone_home_tg_room_id = var.phone_home_tg_room_id
   }))
-}
 
-module "ec2_stockholm" {
-  source = "./ec2"
-
-  instance_name       = var.ec2_name
-  user_data_base64    = local.user_data_base64
   lb_target_group_arn = module.lb_stockholm.target_group_arn
   vpc_zone_identifier = module.vpc_stockholm.subnet_ids
 
@@ -62,8 +60,44 @@ module "ec2_stockholm" {
 module "ec2_ohio" {
   source = "./ec2"
 
-  instance_name       = var.ec2_name
-  user_data_base64    = local.user_data_base64
+  instance_name = var.ec2_name
+
+  user_data_base64 = base64encode(templatefile("user_data.sh.tftpl", {
+    release_url           = var.release_url,
+    release_cookie        = var.release_cookie,
+    primary_host_prefix   = var.primary_host_prefix,
+    secret_key_base       = var.secret_key_base
+    host                  = var.host,
+    check_origin          = var.check_origin,
+    port                  = var.port,
+    dashboard_username    = var.dashboard_username,
+    dashboard_password    = var.dashboard_password,
+    ec2_name              = var.ec2_name,
+    ec2_regions           = join(",", var.ec2_regions)
+    imgproxy_prefix       = var.imgproxy_prefix,
+    imgproxy_key          = var.imgproxy_key,
+    imgproxy_salt         = var.imgproxy_salt,
+    maxmind_license_key   = var.maxmind_license_key,
+    tg_bot_key            = var.tg_bot_key,
+    tg_room_id            = var.tg_room_id,
+    sentry_dsn            = var.sentry_dsn,
+    database_url          = var.ohio_database_url,
+    aws_access_key_id     = var.aws_access_key_id,
+    aws_secret_access_key = var.aws_secret_access_key,
+    aws_s3_bucket         = var.aws_s3_bucket,
+    aws_s3_bucket_static  = var.aws_s3_bucket_static,
+    static_cdn            = var.static_cdn,
+    aws_s3_bucket_events  = var.aws_s3_bucket_events,
+    apns_topic            = var.apns_topic,
+    apns_team_id          = var.apns_team_id,
+    prod_apns_key_id      = var.prod_apns_key_id,
+    prod_apns_key         = var.prod_apns_key,
+    sandbox_apns_key_id   = var.sandbox_apns_key_id,
+    sandbox_apns_key      = var.sandbox_apns_key,
+    phone_home_tg_bot_key = var.phone_home_tg_bot_key,
+    phone_home_tg_room_id = var.phone_home_tg_room_id
+  }))
+
   lb_target_group_arn = module.lb_ohio.target_group_arn
   vpc_zone_identifier = module.vpc_ohio.subnet_ids
 
@@ -82,8 +116,44 @@ module "ec2_ohio" {
 module "ec2_north_california" {
   source = "./ec2"
 
-  instance_name       = var.ec2_name
-  user_data_base64    = local.user_data_base64
+  instance_name = var.ec2_name
+
+  user_data_base64 = base64encode(templatefile("user_data.sh.tftpl", {
+    release_url           = var.release_url,
+    release_cookie        = var.release_cookie,
+    primary_host_prefix   = var.primary_host_prefix,
+    secret_key_base       = var.secret_key_base
+    host                  = var.host,
+    check_origin          = var.check_origin,
+    port                  = var.port,
+    dashboard_username    = var.dashboard_username,
+    dashboard_password    = var.dashboard_password,
+    ec2_name              = var.ec2_name,
+    ec2_regions           = join(",", var.ec2_regions)
+    imgproxy_prefix       = var.imgproxy_prefix,
+    imgproxy_key          = var.imgproxy_key,
+    imgproxy_salt         = var.imgproxy_salt,
+    maxmind_license_key   = var.maxmind_license_key,
+    tg_bot_key            = var.tg_bot_key,
+    tg_room_id            = var.tg_room_id,
+    sentry_dsn            = var.sentry_dsn,
+    database_url          = var.north_california_database_url,
+    aws_access_key_id     = var.aws_access_key_id,
+    aws_secret_access_key = var.aws_secret_access_key,
+    aws_s3_bucket         = var.aws_s3_bucket,
+    aws_s3_bucket_static  = var.aws_s3_bucket_static,
+    static_cdn            = var.static_cdn,
+    aws_s3_bucket_events  = var.aws_s3_bucket_events,
+    apns_topic            = var.apns_topic,
+    apns_team_id          = var.apns_team_id,
+    prod_apns_key_id      = var.prod_apns_key_id,
+    prod_apns_key         = var.prod_apns_key,
+    sandbox_apns_key_id   = var.sandbox_apns_key_id,
+    sandbox_apns_key      = var.sandbox_apns_key,
+    phone_home_tg_bot_key = var.phone_home_tg_bot_key,
+    phone_home_tg_room_id = var.phone_home_tg_room_id
+  }))
+
   lb_target_group_arn = module.lb_north_california.target_group_arn
   vpc_zone_identifier = module.vpc_north_california.subnet_ids
 
@@ -102,8 +172,44 @@ module "ec2_north_california" {
 module "ec2_sydney" {
   source = "./ec2"
 
-  instance_name       = var.ec2_name
-  user_data_base64    = local.user_data_base64
+  instance_name = var.ec2_name
+
+  user_data_base64 = base64encode(templatefile("user_data.sh.tftpl", {
+    release_url           = var.release_url,
+    release_cookie        = var.release_cookie,
+    primary_host_prefix   = var.primary_host_prefix,
+    secret_key_base       = var.secret_key_base
+    host                  = var.host,
+    check_origin          = var.check_origin,
+    port                  = var.port,
+    dashboard_username    = var.dashboard_username,
+    dashboard_password    = var.dashboard_password,
+    ec2_name              = var.ec2_name,
+    ec2_regions           = join(",", var.ec2_regions)
+    imgproxy_prefix       = var.imgproxy_prefix,
+    imgproxy_key          = var.imgproxy_key,
+    imgproxy_salt         = var.imgproxy_salt,
+    maxmind_license_key   = var.maxmind_license_key,
+    tg_bot_key            = var.tg_bot_key,
+    tg_room_id            = var.tg_room_id,
+    sentry_dsn            = var.sentry_dsn,
+    database_url          = var.sydney_database_url,
+    aws_access_key_id     = var.aws_access_key_id,
+    aws_secret_access_key = var.aws_secret_access_key,
+    aws_s3_bucket         = var.aws_s3_bucket,
+    aws_s3_bucket_static  = var.aws_s3_bucket_static,
+    static_cdn            = var.static_cdn,
+    aws_s3_bucket_events  = var.aws_s3_bucket_events,
+    apns_topic            = var.apns_topic,
+    apns_team_id          = var.apns_team_id,
+    prod_apns_key_id      = var.prod_apns_key_id,
+    prod_apns_key         = var.prod_apns_key,
+    sandbox_apns_key_id   = var.sandbox_apns_key_id,
+    sandbox_apns_key      = var.sandbox_apns_key,
+    phone_home_tg_bot_key = var.phone_home_tg_bot_key,
+    phone_home_tg_room_id = var.phone_home_tg_room_id
+  }))
+
   lb_target_group_arn = module.lb_sydney.target_group_arn
   vpc_zone_identifier = module.vpc_sydney.subnet_ids
 
@@ -122,8 +228,44 @@ module "ec2_sydney" {
 module "ec2_sao_paulo" {
   source = "./ec2"
 
-  instance_name       = var.ec2_name
-  user_data_base64    = local.user_data_base64
+  instance_name = var.ec2_name
+
+  user_data_base64 = base64encode(templatefile("user_data.sh.tftpl", {
+    release_url           = var.release_url,
+    release_cookie        = var.release_cookie,
+    primary_host_prefix   = var.primary_host_prefix,
+    secret_key_base       = var.secret_key_base
+    host                  = var.host,
+    check_origin          = var.check_origin,
+    port                  = var.port,
+    dashboard_username    = var.dashboard_username,
+    dashboard_password    = var.dashboard_password,
+    ec2_name              = var.ec2_name,
+    ec2_regions           = join(",", var.ec2_regions)
+    imgproxy_prefix       = var.imgproxy_prefix,
+    imgproxy_key          = var.imgproxy_key,
+    imgproxy_salt         = var.imgproxy_salt,
+    maxmind_license_key   = var.maxmind_license_key,
+    tg_bot_key            = var.tg_bot_key,
+    tg_room_id            = var.tg_room_id,
+    sentry_dsn            = var.sentry_dsn,
+    database_url          = var.sao_paulo_database_url,
+    aws_access_key_id     = var.aws_access_key_id,
+    aws_secret_access_key = var.aws_secret_access_key,
+    aws_s3_bucket         = var.aws_s3_bucket,
+    aws_s3_bucket_static  = var.aws_s3_bucket_static,
+    static_cdn            = var.static_cdn,
+    aws_s3_bucket_events  = var.aws_s3_bucket_events,
+    apns_topic            = var.apns_topic,
+    apns_team_id          = var.apns_team_id,
+    prod_apns_key_id      = var.prod_apns_key_id,
+    prod_apns_key         = var.prod_apns_key,
+    sandbox_apns_key_id   = var.sandbox_apns_key_id,
+    sandbox_apns_key      = var.sandbox_apns_key,
+    phone_home_tg_bot_key = var.phone_home_tg_bot_key,
+    phone_home_tg_room_id = var.phone_home_tg_room_id
+  }))
+
   lb_target_group_arn = module.lb_sao_paulo.target_group_arn
   vpc_zone_identifier = module.vpc_sao_paulo.subnet_ids
 
