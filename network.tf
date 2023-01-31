597e0a164d92c857f3ac5b9fe386ccc34bbbe3aa
ruslandoga <67764432+ruslandoga@users.noreply.github.com>
2023-01-31 19:28:49 +0700

    remove extra regions

--- network.tf
+++ network.tf
@@ -8,16 +8,6 @@ module "vpc_stockholm" {
   }
 }
 
-module "vpc_ohio" {
-  source = "./vpc"
-
-  cidr = "10.1.0.0/16"
-
-  providers = {
-    aws = aws.ohio
-  }
-}
-
 module "vpc_north_california" {
   source = "./vpc"
 
@@ -25,25 +15,5 @@ module "vpc_north_california" {
 
   providers = {
     aws = aws.north_california
-  }
-}
-
-module "vpc_sydney" {
-  source = "./vpc"
-
-  cidr = "10.3.0.0/16"
-
-  providers = {
-    aws = aws.sydney
-  }
-}
-
-module "vpc_sao_paulo" {
-  source = "./vpc"
-
-  cidr = "10.4.0.0/16"
-
-  providers = {
-    aws = aws.sao_paulo
   }
 }
